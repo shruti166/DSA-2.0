2a22a551a572faf64d67f12eed7a80c425843ecc
shruti166 <shrutitech98@gmail.com>
2024-12-29 18:26:19 +0530

    inorder traversal added

--- Tree.java
+++ Tree.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Tree {
     int val;
     Tree left;
@@ -12,6 +15,7 @@ public class Tree {
 }
 
 class CreateTree {
+    public static ArrayList<Integer> arr = new ArrayList<>();
     public static void main(String[] args) {
         // Create the root node
         Tree root = new Tree(2);
@@ -24,5 +28,49 @@ class CreateTree {
         System.out.println("Root value: " + root.val);
         System.out.println("Left child value: " + root.left.val);
         System.out.println("Right child value: " + root.right.val);
+
+
+        ArrayList<Integer> res = new ArrayList<>(Arrays.asList(2, 3, 4, 5));
+        Tree rootRecursive = createTreeRecursive(res, 0);
+        ArrayList<Integer> inorderResult = inorderTraversal(rootRecursive);
+        System.out.println(inorderResult);
+
+        System.out.println("Root value from createTreeRecursive: " + rootRecursive.val);
+
     }
+
+    //Recursively create tree
+
+    public static Tree createTreeRecursive(ArrayList<Integer> arr, int idx) {
+
+        if(idx >= arr.size()) {
+            return null;
+        }
+
+        Tree root = new Tree(arr.get(idx));
+        root.left = createTreeRecursive(arr, 2*idx+1);
+        root.right = createTreeRecursive(arr, 2*idx+2);
+
+
+        return root;
+
+    }
+
+    public static ArrayList<Integer> inorderTraversal(Tree root) {
+        if(root == null) {
+            return null;
+        }
+
+        inorderTraversal(root.left);
+        arr.add(root.val);
+        inorderTraversal(root.right);
+
+        return arr;
+    }
+
+
+
+
 }
+
+
